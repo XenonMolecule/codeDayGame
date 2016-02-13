246dec3ad928c5b50b2f69296a43a6d3df2342c4
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-02-13 18:51:41 +0000

    added canvas and canvas centering

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // JavaScript File
+$(window).on("resize",function(){
+    $("#game").attr("width",700);
+    $("#game").attr("height",700);
+    setTimeout($("#game").css("margin-left",((window.innerWidth/2)-350)),100);
+    console.log(((window.innerWidth/2)-350));
+});
+
+$("#game").css("margin-left",((window.innerWidth/2)-350));
